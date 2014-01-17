cfd972dac86a059082735978e4b1d5de5877e6b4
joe armstrong <joearms@gmail.com>
2014-01-17 10:00:52 +0100

    week4 problems

---
+++ week4_problems.erl
@@ -0,0 +1,33 @@
+-module(week4_problems).
+-export([test/0, dump/2]).
+
+test() ->
+    M = week4_solutions,
+    120 = M:factorial(5),
+    L = [a,b,c,d,e,f],
+    [b,c,d,e,f,a] = M:rotate(1,L),
+    [f,a,b,c,d,e] = M:rotate(-1,L),
+    [d,e,f,a,b,c] = M:rotate(3, L),
+    [e,f,a,b,c,d] = M:rotate(-2, L),
+    [b,c,d,e,f,a] = M:rotate(7,L),
+    [b,c,d,e,f,a] = M:rotate(100000000000000000*6+1,L),
+    "<b>BB</b>"   = M:expand_markup("**BB**"),
+    "<b>BB</b><i>II</i>"   = M:expand_markup("**BB**__II__"),
+    "<b>BB<i>II</i>CC</b>" = M:expand_markup("**BB__II__CC**"),
+    %% the next test is tricky -- comment out if you
+    %% get totally stuck
+    "<b>BB<i>II</i></b><i>CC</i>" = 
+	M:expand_markup("**BB__II**CC__"),
+    test1(),
+    horray.
+
+test1() ->
+    X = epp:parse_file("week4_problems.erl",[],[]),
+    dump("debug", X),
+    8 = week4_solutions:count_atoms(a, X).
+
+dump(File, X) ->
+    {ok, S} = file:open(File, [write]),
+    io:format(S, "~p~n", [X]),
+    file:close(S),
+    io:format("written:~p~n",[File]).
