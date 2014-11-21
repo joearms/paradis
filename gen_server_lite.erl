becc2d8d02e9ba29cf645be4f4108a7f2b7e552b
Joe Armstrong <joearms@gmail.com>
2014-11-21 08:50:36 +0100

    bug fix

--- gen_server_lite.erl
+++ gen_server_lite.erl
@@ -1,23 +1,23 @@
 -module(gen_server_lite).
--export([start/2, loop/2, rpc/2]).
+-export([start/2, loop/3, rpc/2]).
 
 
 start(Mod, State) -> 
-   register(Mod, spawn(gen_server_lite, loop, [Mod, State])).
+   register(Mod, spawn(gen_server_lite, loop, [Mod, State, []])).
 
-loop(Mod, State, LastMessage) ->
+loop(Mod, State, LastMessages) ->
    receive
       {From, Tag, Query} ->
 	   case (catch Mod:handle(Query, State)) of
 	       {'EXIT', Why} ->
-		   error_logger:log_error({From, Query, State}),
+		   error_logger:log_error({From, Query, State, Why}),
 		   exit(From, kill),
-		   loop(Mod, State);
+		   loop(Mod, State, LastMessages);
 	       {Reply, State1} ->
-		   LastMessag1 = add_messare(Query, LastMessage),
-		   statistc_logger:log_this(..)
+		   LastMessages1 = add_message(Query, LastMessages),
+		   statistics_logger:log_this("..."),
 		   From ! {Tag, Reply},
-		   loop(Mod, State1, LastMessage1)
+		   loop(Mod, State1, LastMessages1)
 	   end
        end.
 
@@ -28,3 +28,6 @@ rpc(Mod, Query) ->
        {Tag, Reply} ->
           Reply
     end.
+
+add_message(Q, M) ->
+    [M|Q].
